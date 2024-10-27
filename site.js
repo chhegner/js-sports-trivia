f7daa6fcdd2398747459114c0bc73496f4e03545
Christian Hegner <300070366@fvtc.edu>
2024-10-26 22:47:44 -0500

    Completed Exam 2

--- site.js
+++ site.js
@@ -11,11 +11,38 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 ;(async () => {
 
 	// todo: create your "getNextQuestion" function
+	const https = require('https')
+	const url = `https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple`
+	
+	https.get(url, response => {
+		let body = ''
+		response.on('data', data => body += data.toString())
+		response.on('end', () => console.log(JSON.parse(body)))
+	})
+
+	const getNextQuestion = async json => {
+		const response = await fetch(url)
+		json = await response.json
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		console.log(json)
+		const answers = shuffle([ ...incorrect, correct ])
+		return { question, answers, correct }
+	}
+	
+	const question = await getNextQuestion()
+	console.log(question)
 
 	// todo: create your "renderQuestion" function
 
-	// todo: add the event listener to the "nextQuestion" button
+	const renderQuestion = ({ question, answers, correct }) => { 
 
+
+	}
+
+	// todo: add the event listener to the "nextQuestion" button
+	nextQuestionElement.addEventListener(`click`, event => {
+		setTimeout((getNextQuestion) => {}, 10000)
+	})
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
